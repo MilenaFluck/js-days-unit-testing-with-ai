4e0af6fdb8bc59f4586371ddb558b48c25d3c567
fluck <milena.fluck@adesso.de>
2023-09-24 11:40:20 +0200

    initialisierung des repos für den workshop

---
+++ unit-testing-workspace/apps/unit-testing-backend/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'unit-testing-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/unit-testing-backend',
+};
