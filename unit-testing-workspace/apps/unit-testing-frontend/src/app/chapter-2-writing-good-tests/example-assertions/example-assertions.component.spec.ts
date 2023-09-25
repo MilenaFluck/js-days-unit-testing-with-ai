53065ee137d6333669ede8f7c21d7aaa46c84102
fluck <milena.fluck@adesso.de>
2023-09-25 21:00:20 +0200

    beispiele für regeln 4 und 2

---
+++ unit-testing-workspace/apps/unit-testing-frontend/src/app/chapter-2-writing-good-tests/example-assertions/example-assertions.component.spec.ts
@@ -0,0 +1,50 @@
+import { ExampleAssertionsComponent } from './example-assertions.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+
+describe('ExampleAssertionsComponent', () => {
+
+  let spectator: Spectator<ExampleAssertionsComponent>;
+  const park = 'Jurassic Park';
+
+  const createComponent = createComponentFactory({
+    component: ExampleAssertionsComponent
+  });
+
+  function createEventEmitterMock(): any {
+    return {
+      emit: jest.fn()
+    }
+  }
+
+  beforeEach(() => {
+    spectator = createComponent();
+    spectator.detectChanges();
+  });
+
+
+  /*** Regel 4: Halte die Anzahl an Assertions gering. ***/
+  it('should not emit chosen park', () => {
+    spectator.component.choosePark = createEventEmitterMock();
+    spectator.component.choose(park);
+
+    /*** Welche Assertion würdest du wählen? ***/
+    expect(spectator.component.choosePark.emit).toHaveBeenCalled();
+    expect(spectator.component.choosePark.emit).not.toHaveBeenCalledWith('Phantasialand');
+    expect(spectator.component.choosePark.emit).toHaveBeenCalledTimes(1);
+    expect(spectator.component.choosePark.emit).lastCalledWith(park);
+  });
+
+
+  /*** Regel 2: Teste immer nur eine Sache pro Tests.. ***/
+  it('should not emit chosen park if park exists', () => {
+    spectator.component.choosePark = createEventEmitterMock();
+
+    /*** Test 1 ***/
+    spectator.component.choose(undefined);
+    expect(spectator.component.choosePark.emit).not.toHaveBeenCalled();
+
+    /*** Test 2 ***/
+    spectator.component.choose(park);
+    expect(spectator.component.choosePark.emit).nthCalledWith(1, park);
+  });
+});
---
+++ unit-testing-workspace/apps/unit-testing-frontend/src/app/chapter-2-writing-good-tests/example-assertions/example-assertions.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { ExampleAssertionsComponent } from './example-assertions.component';
+
+@NgModule({
+  declarations: [ExampleAssertionsComponent]
+})
+export class ExampleAssertionsModule {}
