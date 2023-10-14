6256ee24ea3176d671bc24e6ec73810f30cba9fc
Franziska Scheeben <franziska.scheeben@adesso.de>
2023-10-14 17:39:06 +0200

    snapshot test kpi card component

---
+++ unit-testing-workspace/apps/unit-testing-frontend/src/app/dashboard/kpi-card/kpi-card.component.spec.ts
@@ -0,0 +1,32 @@
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { KpiCardComponent } from './kpi-card.component';
+
+describe('KpiCardComponent', () => {
+  let kpiCardComponent: Spectator<KpiCardComponent>;
+
+  const createComponent = createComponentFactory({
+    component: KpiCardComponent
+  });
+
+  beforeEach(() => {
+    kpiCardComponent = createComponent();
+    kpiCardComponent.detectChanges();
+  });
+
+  it('should render kpi card without difference', () => {
+    kpiCardComponent.component.title = 'Besucher';
+    kpiCardComponent.component.value=  10000000;
+    kpiCardComponent.detectChanges();
+
+    expect(kpiCardComponent.element).toMatchSnapshot();
+  });
+
+  it('should render kpi card with difference', () => {
+    kpiCardComponent.component.title = 'Besucher';
+    kpiCardComponent.component.value=  10000000;
+    kpiCardComponent.component.difference=  '10%';
+    kpiCardComponent.detectChanges();
+
+    expect(kpiCardComponent.element).toMatchSnapshot();
+  });
+});
